9542b33c0951a75722849f476023a946a536f8a3
pejj212 <pejj212@gmail.com>
2024-10-05 01:27:31 +0900

    [Baekjoon_11508]2+1세일

---
+++ pejj212/week_02/11508_2+1세일.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); //유제품의 개수
+        int sum = 0;
+        Integer [] arr = new Integer[N];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder()); //내림차순
+
+
+        for(int i=0; i<arr.length; i++) {
+            if(!(i % 3 == 2)) {
+                sum += arr[i];
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
